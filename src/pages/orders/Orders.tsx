20ea9ed00358c70ab6b9dd02fa5c2c146ffb187c
Pankaj <pankajkumarhzb787@gmail.com>
2024-09-22 18:50:49 +0530

    websocket service integartion in orders page

--- src/pages/orders/Orders.tsx
+++ src/pages/orders/Orders.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Breadcrumb, Flex, Form, Space, Table, Tag, Typography } from "antd";
 import { Link } from "react-router-dom";
 import { RightOutlined } from "@ant-design/icons";
@@ -9,28 +9,114 @@ import { getOrders } from "../../http/api";
 import { format } from "date-fns";
 import { orderStatusColors } from "../../constants";
 import { capitalize } from "lodash";
+import socket from "../../lib/socket";
+import { useAuthStore } from "../../store";
 
 const columns = [
-  { title: "Order Id", dataIndex: "_id", key: "_id", render: (_: string, record: Order) => <Typography.Text>{record._id}</Typography.Text> },
-  { title: "Customer", dataIndex: "customerId", key: "customerInfo._id", render: (_: string, record: Order) => <Typography.Text>{record.customerInfo?.firstName} {record.customerInfo?.lastName}</Typography.Text> },
-  { title: "Address", dataIndex: "address", key: "address", render: (_: string, record: Order) => <Typography.Text>{record.address}</Typography.Text> },
-  { title: "Comment", dataIndex: "comment", key: "comment", render: (_: string, record: Order) => <Typography.Text>{record.comment}</Typography.Text> },
-  { title: "Payment Mode", dataIndex: "paymentMode", key: "paymentMode", render: (_: string, record: Order) => <Typography.Text>{record.paymentMode.toUpperCase()}</Typography.Text> },
-  { title: "Payment Status", dataIndex: "paymentStatus", key: "paymentStatus", render: (_: string, record: Order) => <Typography.Text>{capitalize(record.paymentStatus)}</Typography.Text> },
-  { title: "Order Status", dataIndex: "orderStatus", key: "orderStatus", render: (_: string, record: Order) => <Tag bordered={false} color={orderStatusColors[record.orderStatus]}>{capitalize(record.orderStatus)}</Tag> },
-  { title: "Total", dataIndex: "total", key: "total", render: (_: string, record: Order) => <Typography.Text>₹{record.total}</Typography.Text> },
-  { title: "Created At", dataIndex: "createdAt", key: "createdAt", render: (_: string, record: Order) => record.createdAt ? <Typography.Text>{format(new Date(record.createdAt), "dd/mm/yyyy hh:mm a")}</Typography.Text> : null },
-  { title: "Actions", dataIndex: "_id", key: "_id", render: (_: string, record: Order) => <Link to={`/orders/${record._id}`}>Details</Link> },
+  {
+    title: "Order Id",
+    dataIndex: "_id",
+    key: "_id",
+    render: (_: string, record: Order) => (
+      <Typography.Text>{record._id}</Typography.Text>
+    ),
+  },
+  {
+    title: "Customer",
+    dataIndex: "customerId",
+    key: "customerInfo._id",
+    render: (_: string, record: Order) => (
+      <Typography.Text>
+        {record.customerInfo?.firstName} {record.customerInfo?.lastName}
+      </Typography.Text>
+    ),
+  },
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+    render: (_: string, record: Order) => (
+      <Typography.Text>{record.address}</Typography.Text>
+    ),
+  },
+  {
+    title: "Comment",
+    dataIndex: "comment",
+    key: "comment",
+    render: (_: string, record: Order) => (
+      <Typography.Text>{record.comment}</Typography.Text>
+    ),
+  },
+  {
+    title: "Payment Mode",
+    dataIndex: "paymentMode",
+    key: "paymentMode",
+    render: (_: string, record: Order) => (
+      <Typography.Text>{record.paymentMode.toUpperCase()}</Typography.Text>
+    ),
+  },
+  {
+    title: "Payment Status",
+    dataIndex: "paymentStatus",
+    key: "paymentStatus",
+    render: (_: string, record: Order) => (
+      <Typography.Text>{capitalize(record.paymentStatus)}</Typography.Text>
+    ),
+  },
+  {
+    title: "Order Status",
+    dataIndex: "orderStatus",
+    key: "orderStatus",
+    render: (_: string, record: Order) => (
+      <Tag bordered={false} color={orderStatusColors[record.orderStatus]}>
+        {capitalize(record.orderStatus)}
+      </Tag>
+    ),
+  },
+  {
+    title: "Total",
+    dataIndex: "total",
+    key: "total",
+    render: (_: string, record: Order) => (
+      <Typography.Text>₹{record.total}</Typography.Text>
+    ),
+  },
+  {
+    title: "Created At",
+    dataIndex: "createdAt",
+    key: "createdAt",
+    render: (_: string, record: Order) =>
+      record.createdAt ? (
+        <Typography.Text>
+          {format(new Date(record.createdAt), "dd/mm/yyyy hh:mm a")}
+        </Typography.Text>
+      ) : null,
+  },
+  {
+    title: "Actions",
+    dataIndex: "_id",
+    key: "_id",
+    render: (_: string, record: Order) => (
+      <Link to={`/orders/${record._id}`}>Details</Link>
+    ),
+  },
 ];
 
 const Orders = () => {
+  const { user } = useAuthStore();
   const [filterForm] = Form.useForm();
   const [queryParams, setQueryParams] = useState({
     limit: 10,
     page: 1,
   });
 
-  const { data: ordersData } = useQuery<{ data: Order[], total: number, page: number, limit: number, currentPage: number }>({
+  const { data: ordersData } = useQuery<{
+    data: Order[];
+    total: number;
+    page: number;
+    limit: number;
+    currentPage: number;
+  }>({
     queryKey: ["orders", queryParams],
     queryFn: () => {
       const filterdQyeryParams = Object.fromEntries(
@@ -52,8 +138,34 @@ const Orders = () => {
       }))
       .reduce((acc, item) => ({ ...acc, ...item }), {});
 
-      setQueryParams((prev) => ({ ...prev, ...changedFilterFields, page: 1 }));
+    setQueryParams((prev) => ({ ...prev, ...changedFilterFields, page: 1 }));
   };
+
+  useEffect(() => {
+  console.log("useEffect", {user});
+    if (user?.tenant) {
+      socket.on("join", (data) => {
+        console.log("Joined in : ", data.roomId);
+      });
+
+      socket.emit("join", { tenantId: user.tenant });
+
+      socket.on("order-update", (data) => {
+        console.log("Recieved order-update of order: ", data); 
+      })
+    }
+
+    return () => {
+      if(socket.connected) {
+        console.log("Disconnecting socket connection");
+        socket.off("join");
+        socket.off("order-update");
+        // socket.disconnect();
+      }
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Space direction="vertical" size="large" style={{ width: "100%" }}>
       <Flex justify="space-between">
